bab3ccab82ffded6e8b7a364284e060a3ffbd147
Clemens Lieb <vogel612@gmx.de>
2018-12-06 17:44:46 +0100

    Create initial project stubs

---
+++ ReleaseCleaner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReleaseCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
