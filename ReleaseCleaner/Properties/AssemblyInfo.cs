d344c70f843e70e389c012302c9f3d3b7c6a790d
Clemens Lieb <vogel612@gmx.de>
2018-12-08 14:47:34 +0100

    Add InternalsVisibleTo specification for Moq

--- ReleaseCleaner/Properties/AssemblyInfo.cs
+++ ReleaseCleaner/Properties/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 using System.Runtime.CompilerServices;
 
+// expose internals to test project
 [assembly: InternalsVisibleTo("ReleaseCleanerTests")]
+// expose internals to Moq for Mocking them
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
