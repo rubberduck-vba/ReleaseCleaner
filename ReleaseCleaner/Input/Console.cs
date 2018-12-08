46dc0c9e4fa4d2065fa0e57784974dc99cbc6b21
Clemens Lieb <vogel612@gmx.de>
2018-12-08 23:23:27 +0100

    Implement IConsole for realsies

--- ReleaseCleaner/Input/Console.cs
+++ ReleaseCleaner/Input/Console.cs
@@ -1,15 +1,43 @@
 namespace ReleaseCleaner.Input
 {
+    using System;
+    using System.Text;
+    using IO = System.Console;
     internal class Console : IConsole
     {
-        public string ReadPassword(string username)
-        {
-            throw new System.NotImplementedException();
-        }
-
         public string ReadUsername()
         {
-            throw new System.NotImplementedException();
+            IO.WriteLine("Please enter your Github username");
+            // simple ReadLine, because we don't need to mask
+            return IO.ReadLine();
+        }
+
+        // SecureString is recommended to not be used, see platform-compat/DE0001
+        public string ReadPassword(string username)
+        {
+            IO.WriteLine($"Please enter the Github password for {username}");
+            var password = new StringBuilder();
+            ConsoleKeyInfo keyInfo;
+            do
+            {
+                // true intercepts the keypress, effectively masking the password
+                keyInfo = IO.ReadKey(true);
+                if (keyInfo.Key == ConsoleKey.Backspace)
+                {
+                    if (keyInfo.Modifiers.HasFlag(ConsoleModifiers.Control))
+                    {
+                        password.Clear();
+                    }
+                    else
+                    {
+                        password.Remove(password.Length - 1, 1);
+                    }
+                }
+                // for all other cases
+                password.Append(keyInfo.KeyChar);
+            } while (keyInfo.Key != ConsoleKey.Enter);
+            IO.WriteLine();
+            return password.ToString();
         }
     }
 
