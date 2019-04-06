ca64966829919c616fd815da82baa8c19f63d1f3
Clemens Lieb <vogel612@gmx.de>
2019-04-06 21:00:56 +0200

    Add basic unit-tests for message indicating unknown arguments

--- ReleaseCleanerTests/Invocation/ParserTests.cs
+++ ReleaseCleanerTests/Invocation/ParserTests.cs
@@ -11,7 +11,7 @@ namespace ReleaseCleaner.Invocation
     [TestFixture]
     [Category("UnitTest")]
     [Category("Argument Parsing")]
-    public class ParserTests 
+    public class ParserTests
     {
         [Test]
         [TestCase("")]
@@ -29,13 +29,38 @@ namespace ReleaseCleaner.Invocation
         [TestCase("--match v2.2.0.* --invert")]
         [TestCase("--match v2.2.0.* --invert --owner rubberduck-vba")]
         [TestCase("--match v2.2.0.* --invert --project Rubberduck")]
-        public void EmptyArgs_IsRejected(string commandLine) 
+        public void InsufficientArgs_IsRejected(string commandLine)
         {
-            Assert.Throws<ArgumentException>(() => CUT.Parse(commandLine.Split(' '), null));
+            var console = new Mock<IConsole>();
+            Assert.Throws<ArgumentException>(() => CUT.Parse(commandLine.Split(' '), console.Object));
         }
 
         [Test]
-        [TestCase("rubberduck-vba/Rubberduck -m 2.2.0*", "rubberduck-vba", "Rubberduck", false, new string[]{"2.2.0*"})]
+        [TestCase("--foo", new[]{0})]
+        [TestCase("--foo --bar", new[]{0,1})]
+        [TestCase("rubberduck-vba/Rubberduck --foo", new[]{1})]
+        public void UnknownArgument_ProvokesMessage(string commandLine, int[] unknownIndices)
+        {
+            var console = new Mock<IConsole>();
+            var parts = commandLine.Split(' ');
+            console.Setup(c => c.Write(It.IsAny<string>()))
+                .Verifiable();
+            try
+            {
+                CUT.Parse(parts, console.Object);
+            }
+            catch (Exception)
+            {
+                // this is not what we're testing here, so Exceptions during parsing are ignored
+            }
+            foreach (var index in unknownIndices)
+            {
+                console.Verify(c => c.Write(It.IsRegex($".*{parts[index]}.*")), Times.Once());
+            }
+        }
+
+        [Test]
+        [TestCase("rubberduck-vba/Rubberduck -m 2.2.0*", "rubberduck-vba", "Rubberduck", false, new string[] { "2.2.0*" })]
         public void ValidArgs_AreCorrectlyParsed(string commandLine, string owner, string repo, bool invert, string[] matchers)
         {
             var console = new Mock<IConsole>();
